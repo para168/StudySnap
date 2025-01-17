e300af1a52c0d0252aa0c3dc2f9a1453a3a96fd4
para168 <paras.patil850@gmail.com>
2025-01-18 00:36:00 +0530

    Delete src directory

--- src/data/subjects/index.ts
+++
@@ -1,9 +0,0 @@
-import { physicsChapters } from './physics';
-import { chemistryChapters } from './chemistry';
-import { mathematicsChapters } from './mathematics';
-
-export const subjectsData = {
-  Physics: physicsChapters,
-  Chemistry: chemistryChapters,
-  Mathematics: mathematicsChapters,
-};
--- src/data/subjects/physics/chapters/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './physicsAndMeasurement';
-export * from './kinematics';
-export * from './lawsOfMotion';
-export * from './workEnergyPower';
